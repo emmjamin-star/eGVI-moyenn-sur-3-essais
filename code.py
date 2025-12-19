e2500d188fcb79ee472d1c8398cef42ee0cf9692
emmjamin-star <emm.jamin@gmail.com>
2025-12-19 10:41:44 +0100

    Fix indentation for eGVI score interpretation message

--- code.py
+++ code.py
@@ -407,7 +407,7 @@ if st.button("Lancer le calcul du score eGVI"):
 
         st.markdown("### 📊 Résultats du score eGVI")
         st.write(f"**Score eGVI** : {eGVI:.2f}")
-        st.write(f"**Lecture du test** : Un individu présentant une marche saine aura un score compris entre 98 et 102. Tout score en-dehors indique une atteinte à la variabilité de la marche."
+        st.write(f"**Lecture du test** : Un individu présentant une marche saine aura un score compris entre 98 et 102. Tout score en-dehors indique une atteinte à la variabilité de la marche.")
        
     except Exception as e:
         st.error(f"Erreur pendant l'analyse : {e}")
